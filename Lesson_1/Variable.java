4b942456525e219d6b214705672ed691d4fc0519
Ax\0m <d-343@yandex.ru>
2021-11-06 21:40:58 +0300

    add2works

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,26 @@
+public class Variable{
+    public static void main(String[] args) {
+    
+    byte windows = 10;
+    short service = 1910;
+    int oper = 2666;
+    long Cpucan = 1999999999;
+    float vcard = 20.60f;
+    double gh = 4.45612;
+    char vname = 'N';
+    boolean cool = true;
+    String compname = "Hasee";
+    
+    System.out.println(windows);
+    System.out.println(service);
+    System.out.println(oper);
+    System.out.println(Cpucan);
+    System.out.println(vcard);
+    System.out.println(gh);
+    System.out.println(vname);
+    System.out.println(cool);
+    System.out.println(compname);
+
+
+    }
+}
