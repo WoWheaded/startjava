a2a005c97d3067cfc521f2a93240d059247f907c
Ax\0m <d-343@yandex.ru>
2021-11-13 00:37:09 +0300

    addSpaces

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -3,12 +3,14 @@ public class Cycle{
          for(int f = 0; f <=20; f++) {
              System.out.println(f);
          }
+         System.out.println(" ");
 
         int w = 6;
         while(w >= -6){
             System.out.println(w);
             w-=2;
         }
+        System.out.println(" ");
 
         int a = 10;
         int sum = 0;
@@ -19,10 +21,5 @@ public class Cycle{
             a++;
         }while (a <= 20);
         System.out.println("Сумма нечетных чисел с 10 до 20 = " + sum);
-
-        
-
-
-
     }
 }
