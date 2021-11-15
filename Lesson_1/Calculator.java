f9a462aa9851b38e8aa32029b0052fec5e2fdb18
Ax\0m <d-343@yandex.ru>
2021-11-15 05:48:28 +0300

    addCalculator

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,35 @@
+    import java.util.Scanner;
+
+    public class Calculator {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int num1 = console.nextInt();
+        System.out.println("Введите второе число");
+        int num2 = console.nextInt();
+        System.out.println("введите знак");
+        char c = console.next().charAt(0);
+        int result = 1;
+        
+        while (num1 >= 0 && num2 >= 0) {
+        if (c == '+') {
+            System.out.println(num1 + num2);
+        } else if (c == '-') {
+            System.out.println(num1 - num2);
+        } else if (c == '*') {
+            System.out.println(num1 * num2);
+        } else if (c == '^') {
+            for(int a =0; a < num2; a++) {
+                result = result * num1;
+                } System.out.print(result);
+        } else if (c == '%') {
+            System.out.println(num1 % num2);
+        } else {
+            System.out.println("Thanks");
+        } 
+        break;
+        }
+    }
+}
+
+
