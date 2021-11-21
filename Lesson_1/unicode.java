29043b033d4a0d3c98ffd9def7762989dac219fd
Ax\0m <d-343@yandex.ru>
2021-11-21 23:10:08 +0300

    renameClass

--- Lesson_1/unicode.java
+++
@@ -1,8 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        for (char symbol = 33; symbol <= 126; symbol++) {
-            int num = symbol;
-            System.out.println(num + "\n" + symbol + "\n" + " ");
-            }
-    }
-}
