88ec126612a083871adfde191122bf883c783339
Ax\0m <d-343@yandex.ru>
2021-11-30 17:50:02 +0300

    remove

--- Lesson_1/Unicode.java
+++
@@ -1,8 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        for (char symbol = 9398; symbol <= 10178; symbol++) {
-            int num = symbol;
-            System.out.print(num + "\n" + symbol + "\n" + "  ");
-        }
-    }
-}
