2a507be12ba8cdd024274728e69b2bcf733292e5
WoWheaded <93683348+WoWheaded@users.noreply.github.com>
2021-11-30 17:22:33 +0300

    Add files via upload

---
+++ Lesson_2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Dmitry";
+    float height = 18.1f;
+    float weight = 74.3f;
+    int age = 27;
+
+    void move() {
+    }
+
+    void sit() {
+    }
+
+    void run() {
+    }
+
+    void talk() {
+    }
+
+    void learnJava() {
+    }
+}
