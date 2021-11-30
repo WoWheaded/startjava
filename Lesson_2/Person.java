e575db7cba31935e0c7eaeb4a50120773f2133a5
Ax\0m <d-343@yandex.ru>
2021-11-30 17:36:08 +0300

    rework

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -21,29 +21,4 @@ public class Person {
 
     void learnJava() {
     }
-    
-=======
-public class Person {
-
-    String gender = "male";
-    String name = "Dmitry";
-    float height = 18.1f;
-    float weight = 74.3f;
-    int age = 27;
-
-    void move() {
-    }
-
-    void sit() {
-    }
-
-    void run() {
-    }
-
-    void talk() {
-    }
-
-    void learnJava() {
-    }
->>>>>>> 2a507be12ba8cdd024274728e69b2bcf733292e5
 }
