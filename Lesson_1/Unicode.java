85baf3889ad8cae549eb9590995ef687c765da20
Ax\0m <d-343@yandex.ru>
2021-11-30 17:26:00 +0300

    q

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,8 +1,8 @@
 public class Unicode {
     public static void main(String[] args) {
-        for (char symbol = 33; symbol <= 126; symbol++) {
+        for (char symbol = 9398; symbol <= 10178; symbol++) {
             int num = symbol;
-            System.out.println(num + "\n" + symbol + "\n" + "  ");
+            System.out.print(num + "\n" + symbol + "\n" + "  ");
         }
     }
 }
--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -20,4 +20,5 @@ public class Person {
 
     void learnJava() {
     }
+    
 }
