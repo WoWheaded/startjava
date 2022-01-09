136276911fd85ee74f6b57bf8e4d7d588ecf55d6
WoWheaded <Swimming232!>
2022-01-09 18:57:29 +0300

    add game for lesson4

--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -1,11 +1,8 @@
 package com.startjava.lesson_2_3_4.game;
 
-import java.util.Arrays;
-
 public class Player {
     private String name;
-    private int count;
-    private int[] attempts = new int[5];
+    private int number;
 
     public Player(String name) {
         this.name = name;
@@ -15,24 +12,11 @@ public class Player {
         return name;
     }
 
-    public int[] getAttempts() {
-        return Arrays.copyOf(attempts, count);
+    public int getNumber() {
+        return number;
     }
 
-    public void setAttempts(int playerGuess) {
-        attempts[count] = playerGuess;
-        count++;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public void clearAttempts() {
-        Arrays.fill(attempts, 0, count, 0);
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
