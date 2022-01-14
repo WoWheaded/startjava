79c3e7a63ea019de37a95ece52566767a74be281
WoWheaded <Swimming232!>
2022-01-14 16:28:39 +0300

    add7

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,13 +1,13 @@
 --  create database
 
 CREATE TABLE jaegersInfo (
-	id                         SERIAL PRIMARY KEY,
-	modelName          TEXT,
-	mark                    CHAR(6),
-	height                  INTEGER,
-	weight                 REAL,
-	status                  TEXT,
-	origin                   TEXT,
-	launch                  DATE,
-	kaijukil                 INTEGER
+ id		SERIAL PRIMARY KEY,
+ modelName		TEXT,
+ mark		CHAR(6),
+ height		INTEGER,
+ weight		 REAL,
+ status		TEXT,
+ origin		TEXT,
+ launch		DATE,
+ kaijuki		INTEGER
 );
