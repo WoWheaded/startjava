3b13ce2bcdee7274d427369fc68f60e4acb3b0ef
WoWheaded <93683348+WoWheaded@users.noreply.github.com>
2022-01-14 16:35:29 +0300

    Update create_db.sql

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -2,12 +2,12 @@
 
 CREATE TABLE jaegersInfo (
  id		SERIAL PRIMARY KEY,
- modelName	TEXT,
- mark		CHAR(6),
- height		INTEGER,
- weight		 REAL,
- status		TEXT,
- origin		TEXT,
- launch		DATE,
- kaijuki	INTEGER
+ modelName    TEXT,
+ mark         CHAR(6),
+ height		     INTEGER,
+ weight		     REAL,
+ status		     TEXT,
+ origin		     TEXT,
+ launch		     DATE,
+ kaijuki	     INTEGER
 );
